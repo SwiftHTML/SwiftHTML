d4bfed7e01ee835b29796cfd6352c15883ec1416
Piotr Jeremicz <piotrek@jeremicz.com>
2025-03-10 00:32:51 +0100

    Updated `Node` documentation

--- Source/SwiftHTML/Node/Node.swift
+++ Source/SwiftHTML/Node/Node.swift
@@ -9,8 +9,17 @@
 ///
 /// Node can contain children that are represented by the `associatedtype Body`. `Body` inherits
 /// from the Node protocol, which provides full support for nested children.
+/// 
 public protocol Node: CustomStringConvertible {
+    /// The type of node representing the body of this node.
+    ///
+    /// When you create a custom node, Swift infers this type from your
+    /// implementation of the required `var body: some Node` property.
     associatedtype Body: Node
     
+    /// The content and behavior of the node.
+    ///
+    /// When you implement a custom node, you must implement a computed
+    /// `body` property to provide the content for your node.
     var body: Self.Body { get }
 }
