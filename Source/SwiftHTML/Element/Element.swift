48a124356edbd4606b102e45deb497e7263a00e3
Piotr Jeremicz <piotrek@jeremicz.com>
2025-03-29 23:52:54 +0100

    Created `Element` abstraction

---
+++ Source/SwiftHTML/Element/Element.swift
@@ -0,0 +1,37 @@
+//
+//  Element.swift
+//  SwiftHTML
+//
+//  Created by Piotrek Jeremicz on 28.03.2025.
+//
+
+/// A protocol representing an HTML-like element with a tag name.
+/// Conforms to `Node` and provides default implementations for rendering.
+public protocol Element: Node {
+    
+    /// The HTML tag name of the element.
+    var tag: String { get }
+}
+
+public extension Element {
+    var tag: String {
+        "\(type(of: self))".lowercased()
+    }
+    
+    var description: String {
+        "<\(tag)>\(body.description)</\(tag)>"
+    }
+}
+
+public extension Element where Body == Never {
+    var description: String {
+        "<\(tag)/>"
+    }
+    
+    /// Prevents access to the `body` property for elements that do not support it.
+    /// Triggers a runtime error if accessed.
+    @_spi(Private)
+    var body: Never {
+        fatalError("\(type(of: self)) does not support the `body` property.")
+    }
+}
